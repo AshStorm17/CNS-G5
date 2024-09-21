0e49b6e14f0191583330868ca0dcb59bcfd9c5f4
AshStorm17 <114629585+AshStorm17@users.noreply.github.com>
2024-09-22 00:58:54 +0530

    Bank Running updates

--- run_bank.sh
+++ run_bank.sh
@@ -1,13 +1,35 @@
 #!/bin/bash
 
+# Default values
+PORT=8080
+AUTH_FILE="bank.txt"
+
+# Parse command-line arguments for port (-p) and auth file (-s)
+while getopts ":p:s:" opt; do
+  case ${opt} in
+    p )
+      PORT=$OPTARG
+      ;;
+    s )
+      AUTH_FILE=$OPTARG
+      ;;
+    \? )
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+    : )
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
+
+# Certificate Generation
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout server.key -out server.crt
+
 # Compile the bank server
-g++ -o bank bank.cpp -lmysqlcppconn
+g++ -std=c++11 bank.cpp -o bank.o -lssl -lcrypto -lmysqlcppconn -lpthread
 
-# Compile the atm client
-g++ -o atm atm.cpp -lmysqlcppconn
-
-# Run the bank server
-./bank
-
-# Run the ATM client
-./atm
+# Run the bank server with the specified port and auth file
+echo "Running bank server on port $PORT"
+./bank.o -p "$PORT" -s "$AUTH_FILE"
